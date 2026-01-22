c359d973bdf93092aaa19d9bdb0859b700b971a4
David <david.adedeji@ibm.com>
2026-01-22 11:45:54 +0000

    Added logic for non-integer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def fizzbuzz(value:int):
-
+    if not isinstance(value,int):
+        raise TypeError("Value has to be an integer")
     if value % 3 ==0 and value % 5 ==0 :
         return "FizzBuzz"
     
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -13,4 +13,12 @@ def test_return_buzz_multiple_of_5():
 
 def test_return_fizzbuzz_multiple_of_3_and_5():
     assert fizzbuzz(15)=="FizzBuzz"
+
+def test_for_non_integers():
+    with pytest.raises(TypeError):
+        fizzbuzz("15")
+    with pytest.raises(TypeError):
+        fizzbuzz(3.0)
+    with pytest.raises(TypeError):
+        fizzbuzz(None)
     
