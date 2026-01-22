c819d0d6a4debef01e8ab65691d05edeaac2934c
David <david.adedeji@ibm.com>
2026-01-22 11:43:02 +0000

    Added fizz buzz logic

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def fizzbuzz(value:int):
 
-    if value %3 ==0:
+    if value % 3 ==0 and value % 5 ==0 :
+        return "FizzBuzz"
+    
+    if value % 3 ==0:
         return "Fizz"
     
-    if value %5 ==0:
+    if value % 5 ==0:
         return "Buzz"
     
     return str(value)
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -8,8 +8,9 @@ def test_smoke_test():
 def test_return_fizz_multiple_of_3():
     assert fizzbuzz(3)=="Fizz"
 
-def test_return_buzzz_multiple_of_5():
+def test_return_buzz_multiple_of_5():
     assert fizzbuzz(5)=="Buzz"
 
-
+def test_return_fizzbuzz_multiple_of_3_and_5():
+    assert fizzbuzz(15)=="FizzBuzz"
     
